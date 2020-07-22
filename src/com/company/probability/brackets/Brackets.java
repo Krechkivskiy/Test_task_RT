ef59578fed21b21fa5102c4dafccbb140ad53b86
svistylin <vitalik251781@gmail.com>
2020-07-22 21:46:01 +0300

    moved Brackets to own package

--- src/com/company/probability/brackets/Brackets.java
+++ src/com/company/probability/brackets/Brackets.java
@@ -38,8 +38,8 @@ public class Brackets {
         }
         // Check for two conditions:
         // First: if count of '(' will not be less than right and we will print ')' we will get result like this '())('
-        // by task conditions it's incorrect
-        // Second: There are one case when count of '(' can be less than '(' -- last symbol in string
+        // by task conditions it's wrong
+        // Second: There are one case when count when count of'(' can be less than '(' -- last symbol in string
         if (reservedLeftBracketsCount < reservedRightBracketsCount ||
                 (reservedLeftBracketsCount == 0 && reservedRightBracketsCount != 0)) {
             StringBuilder cur = new StringBuilder(currentCombination.toString());
