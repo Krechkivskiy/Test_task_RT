3f3910a94a82eeb53dae9f88f1f6e8bfc393c000
svistylin <vitalik251781@gmail.com>
2020-07-23 12:08:58 +0300

    some fixes

--- src/com/company/probability/factorial/Factorial.java
+++ src/com/company/probability/factorial/Factorial.java
@@ -1,13 +1,10 @@
 package com.company.probability.factorial;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Factorial {
     public static void main(String[] args) {
         int result = 0;
-        List<Long> digits = new ArrayList<>();
         BigInteger factorial = findFactorial(100);
         char[] chars = factorial.toString().toCharArray();
         for (char aChar : chars) {
