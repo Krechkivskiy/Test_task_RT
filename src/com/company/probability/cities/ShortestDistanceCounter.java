475eba5d1d627da022f32576283f8815f49f056c
svistylin <vitalik251781@gmail.com>
2020-07-23 11:08:59 +0300

    some fixes

--- src/com/company/probability/cities/ShortestDistanceCounter.java
+++ src/com/company/probability/cities/ShortestDistanceCounter.java
@@ -3,17 +3,18 @@ package com.company.probability.cities;
 import java.util.Set;
 
 public class ShortestDistanceCounter {
-   private int minWay = 10000;
+    private int minWay = 10000;
+
     public ShortestDistanceCounter() {
     }
 
     public void executeSearch(String route) {
-
         CityDatabase db = CityDatabaseFactory.getDb();
         String[] res = route.split(" ");
         City from = db.findByName(res[0]);
         City to = db.findByName(res[1]);
         compute(from, to, 0);
+        System.out.println(minWay);
     }
 
     // Method which looks for the shortest way from one city to other
@@ -21,9 +22,9 @@ public class ShortestDistanceCounter {
         Set<City> cities = from.getNeighbors().keySet();
         for (City city : cities) {
             if (city.equals(to)) {
-    if (from.getNeighbors().get(city)<minWay){
-        minWay=from.getNeighbors().get(city);
-    }
+                if (from.getNeighbors().get(city) < minWay) {
+                    minWay = from.getNeighbors().get(city);
+                }
                 break;
             } else {
                 compute(city, to, currentLength + from.getNeighbors().get(city));
